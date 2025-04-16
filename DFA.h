de802955194bab0d8819d0fb5a9a3530ccdcc2ed
Areeza971 <areezabashir@gmail.com>
2025-04-16 10:34:23 +0500

    Update DFA.h

--- DFA.h
+++ DFA.h
@@ -1,19 +1,28 @@
 #include <vector>
-//This is my representation of a DFA. It uses unordered maps so it can also be a partial DFA
-class DFA{
+#include <iostream>
+using namespace std;
+
+// This is my representation of a DFA. It uses vectors to store transitions.
+class DFA {
 public:
-	DFA(int num_states, int alph_size, int start_index, std::vector<bool> finals, std::vector<int> H, std::vector<int> L, std::vector<int> T);
-	void print_DFA();
-	//number of states
-	int Q;
-	//whether a state is final
-	std::vector<bool> final;
-	//starting state
-	int q0;
-	//size of alphabet; a letter is an integer from 0 to alph_size-1
-	int alph_size;
-	// transitions[i] maps j to k iff there exists a transition from i to k under letter j
-	std::vector<int> Heads;
-	std::vector<int> Labels;
-	std::vector<int> Tails;
+    DFA(int num_states, int alph_size, int start_index, vector<bool> finals, vector<int> H, vector<int> L, vector<int> T);
+    void print_DFA();
+
+    // Number of states
+    int Q;
+
+    // Whether a state is final
+    vector<bool> final;
+
+    // Starting state
+    int q0;
+
+    // Size of alphabet; a letter is an integer from 0 to alph_size - 1
+    int alph_size;
+
+    // transitions[i] represents:
+    // from Tails[i], under input Labels[i], go to Heads[i]
+    vector<int> Heads;
+    vector<int> Labels;
+    vector<int> Tails;
 };
